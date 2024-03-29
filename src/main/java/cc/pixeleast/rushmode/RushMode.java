f41b1d0fd648a33d92597c8eaa2acaacec2f707b
ican <lsy20113@163.com>
2024-03-29 19:50:02 +0800

    Initial commit

---
+++ src/main/java/cc/pixeleast/rushmode/RushMode.java
@@ -0,0 +1,16 @@
+package cc.pixeleast.rushmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RushMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
