29dd8aa08ff578cd0137a7ba4b1484fba5da9af7
ican <lsy20113@163.com>
2024-04-19 21:19:18 +0800

    Replace unsupported text blocks

--- src/main/java/cc/pixeleast/rushmode/configuration/Config.java
+++ src/main/java/cc/pixeleast/rushmode/configuration/Config.java
@@ -35,15 +35,13 @@ public class Config extends ConfigManager {
 
         // Due to the poor design of 1058, even if the default upgrade forge/miner has tier 4/2, it must be reduced by one
         // This is counterintuitive, implementing shop/upgrade improvements is urgent
-        yml.setComment(GENERAL_TEAM_UPGRADES, """
-                List of team upgrades and their tiers
-                Format: Upgrade Name, Tier
-                Note: Upgrades in the following list will display as unlocked, but they will not actually have any effect""");
+        yml.setComment(GENERAL_TEAM_UPGRADES, "List of team upgrades and their tiers\n" +
+                "Format: Upgrade Name, Tier\n" +
+                "Note: Upgrades in the following list will display as unlocked, but they will not actually have any effect");
         yml.addDefault(GENERAL_TEAM_UPGRADES, Arrays.asList("upgrade-forge,3", "upgrade-miner,1"));
-        yml.setComment(GENERAL_TEAM_EFFECTS, """
-                List of team effects and their amplifiers/durations
-                Format: Effect Name, Level, Duration (0 is infinite)
-                For example, after disabling upgrade-miner in the options above, corresponding effects can be added here""");
+        yml.setComment(GENERAL_TEAM_EFFECTS, "List of team effects and their amplifiers/durations\n" +
+                "Format: Effect Name, Level, Duration (0 is infinite)\n" +
+                "For example, after disabling upgrade-miner in the options above, corresponding effects can be added here");
         yml.addDefault(GENERAL_TEAM_EFFECTS, Arrays.asList("SPEED,1,0", "FAST_DIGGING,1,0"));
 
         yml.setComment(GENERAL_BRIDGE_DELAY, "Delay for placing each block");
@@ -53,9 +51,8 @@ public class Config extends ConfigManager {
         yml.setComment(GENERAL_BRIDGE_CONSUMING_BLOCKS, "Whether to consume the number of blocks built");
         yml.addDefault(GENERAL_BRIDGE_CONSUMING_BLOCKS, false);
 
-        yml.setComment(GENERAL_BED_PROTECTION, """
-                The defense layer of the bed
-                Corresponding versions of materials, check https://helpch.at/docs/{version}/org/bukkit/Material""");
+        yml.setComment(GENERAL_BED_PROTECTION, "The defense layer of the bed\n" +
+                "Corresponding versions of materials, check https://helpch.at/docs/{version}/org/bukkit/Material");
         yml.addDefault(GENERAL_BED_PROTECTION, Arrays.asList("WOOD", "WOOL", "STAINED_GLASS"));
 
         yml.setComment(GENERAL_BANNED_ITEMS, "List of items that cannot be purchased in Rush Mode");
@@ -64,9 +61,8 @@ public class Config extends ConfigManager {
         yml.addDefault(GENERAL_EVENTS, Arrays.asList("BEDS_DESTROY, 900", "ENDER_DRAGON, 2100", "GAME_END, 2340"));
 
         // 1058 has implemented Sounds internally, but the API is not open to the public
-        yml.setComment(SOUNDS, """
-        Manage sound in various situations
-        Corresponding versions of sounds, check https://helpch.at/docs/{version}/org/bukkit/Sound""");
+        yml.setComment(SOUNDS, "Manage sound in various situations\n" +
+        "Corresponding versions of sounds, check https://helpch.at/docs/{version}/org/bukkit/Sound");
         yml.setComment(SOUNDS_BRIDGE, "The sound played when placing each block");
         yml.addDefault(SOUNDS_BRIDGE, "STEP_WOOL");
         yml.setComment(SOUNDS_CANT_BUY, "The sound of players purchasing banned items");
